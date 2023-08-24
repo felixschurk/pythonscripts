b60c4a0cd481f62f3ecdb74b324ccc58fa5103e6
Felix Schurk <felix.schurk@rwth-aachen.de>
2023-08-24 13:56:59 +0200

    update scripts and kickout connect ide commented

--- startParaView.sh
+++ startParaView.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+# Calls the script on the cluster, which loads the moduels and creates the pvserver
 
-ssh schurk1@juwelsvis03.fz-juelich.de "~/Public/startParaviewServer.sh" &
+ssh schurk1@juwelsvis03.fz-juelich.de "~/Public/startParaViewServer.sh" &
 
-sleep 5
-
-# CONNECT_ID=$(ssh juwelsvis03.fz-juelich.de "cat ~/Public/.paraviewConnectID.txt")
+sleep 3
 
 ssh -N -L 12347:localhost:12347 schurk1@juwelsvis03.fz-juelich.de &
 
-paraview # --connect-id=$CONNECT_ID
+paraview
--- startParaViewServer.sh
+++ startParaViewServer.sh
@@ -12,12 +12,6 @@
 ml purge
 ml Stages/2023 GCC ParaStationMPI ParaView/5.11.0-EGL ParaViewPlugin-Nek5000/20230208-EGL
 
-#Connect-Id must be of type integer
-# CONNECTID=$(shuf -i 100000-999999 -n 1)
-# echo "Your connection ID:" $CONNECTID
 
-#Write Connect ID to file
-# echo "$CONNECTID" > $HOME/Public/.paraviewConnectID.txt
-
-pvserver --sp=12347 # --no-mpi --disable-xdisplay-test # --connect-id=$CONNECTID "$@"
+pvserver --sp=12347
 
