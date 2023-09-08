a4434452516dd74b87da073197457d60291594b5
Felix Schurk <felix.schurk@rwth-aachen.de>
2023-09-08 09:28:28 +0200

    add new files

---
+++ run_mpi_local.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+# # module load mpi/mpich-x86_64
+
+export CATALYST_DEBUG=ON
+export CATALYST_DATA_DUMP_DIRECTORY="/home/felix/data/juelich/ippl_toolchain/ippl/catalyst_dump/"
+export CATALYST_IMPLEMENTATION_PATHS="/home/felix/data/juelich/ParaView-5.11.1-MPI-Linux-Python3.9-x86_64/lib/catalyst"
+export CATALYST_IMPLEMENTATION_NAME="paraview"
+export PARAVIEW_CATALYST_DIR="/home/felix/data/juelich/ParaView-5.11.1-MPI-Linux-Python3.9-x86_64/lib/catalyst"
+
+mpirun -np 2 ./build_serial/test/stream/TestCatalystAdaptor ./test/stream/catalyst_pipeline.py
+
