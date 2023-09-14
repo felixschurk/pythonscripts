27de72229e89673bb86e1d1f06a62ac1a2fa49fc
Felix Schurk <felix.schurk@rwth-aachen.de>
2023-09-14 14:25:06 +0200

    remove python files

--- local_env_master.sh
+++
@@ -1,6 +0,0 @@
-export CATALYST_DEBUG=ON
-export CATALYST_DATA_DUMP_DIRECTORY="/home/felix/data/juelich/ippl_toolchain/ippl/catalyst_dump/"
-export CATALYST_IMPLEMENTATION_PATHS="/home/felix/data/juelich/ParaView-master-5.11.1-1654-gafb2f0006a-MPI-Linux-Python3.9-x86_64/lib/catalyst/"
-export CATALYST_IMPLEMENTATION_NAME="paraview"
-export PARAVIEW_CATALYST_DIR="/home/felix/data/juelich/ParaView-master-5.11.1-1654-gafb2f0006a-MPI-Linux-Python3.9-x86_64/lib/catalyst/"
-
