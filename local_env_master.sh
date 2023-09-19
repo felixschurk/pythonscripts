b17607413453fc034bd47ae1eaee62044b4d1623
Felix Schurk <felix.schurk@rwth-aachen.de>
2023-09-19 10:43:33 +0200

    add settings for master

---
+++ local_env_master.sh
@@ -0,0 +1,6 @@
+ml mpi/mpich-x86_64
+# export CATALYST_DEBUG=ON
+export CATALYST_IMPLEMENTATION_PATHS="/home/felix/data/juelich/ParaView-master-5.11.1-1716-g29db1b0a7d-MPI-Linux-Python3.9-x86_64/lib/catalyst/"
+export CATALYST_IMPLEMENTATION_NAME="paraview"
+export PARAVIEW_CATALYST_DIR=${CATALYST_IMPLEMENTATION_PATHS}
+
